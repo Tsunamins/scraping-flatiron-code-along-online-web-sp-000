f0cb4ac2f0457eadaed7279e567b308119da406d
Alison Reilly <reillyamr@gmail.com>
2019-01-07 17:54:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,11 @@ class Scraper
   
   def get_page 
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    binding.pry
+    #binding.pry
+    
+    doc.css(".post").each do |post|
+      course = Course.new 
+      course.title
     
   end 
   
